17739a805dc3fc13c1737bce155e99bc99d40668
CapralJack <larionov.anton85@gmail.com>
2020-01-07 01:26:01 +0300

    'deploy'

---
+++ dist/precache-manifest.1767c9378de9d50e08673a3fbea5107f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f9af9b8d8dac95626f",
+    "url": "/css/chunk-804f4c26.5c08852e.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "38f9af9b8d8dac95626f",
+    "url": "/js/chunk-804f4c26.4003a9c2.js"
+  },
+  {
+    "revision": "a467b6f00fca0c67b9bf",
+    "url": "/js/chunk-2d0d05dc.8e316e57.js"
+  },
+  {
+    "revision": "eb637c35adef3c4f8fae",
+    "url": "/js/chunk-2d21a3d2.60e15763.js"
+  },
+  {
+    "revision": "9c424ac0fef66c7bd8e1",
+    "url": "/js/chunk-3be11118.e4173c63.js"
+  },
+  {
+    "revision": "e9c5d389054aee3ff297",
+    "url": "/js/chunk-4e668e14.c4ed84ff.js"
+  },
+  {
+    "revision": "13513864b627485d81b5",
+    "url": "/js/chunk-5f4ad635.cf7eb3f9.js"
+  },
+  {
+    "revision": "4590739eae246b3a17bc",
+    "url": "/js/chunk-vendors.fae8f42d.js"
+  },
+  {
+    "revision": "4107321ecbec825131be",
+    "url": "/js/chunk-2d217a99.d3632f04.js"
+  },
+  {
+    "revision": "ed3fd75745f27fdc8d4f",
+    "url": "/js/chunk-9d1675fc.26a2a304.js"
+  },
+  {
+    "revision": "e4ba418aa656f1ab2ec4",
+    "url": "/js/chunk-c420df12.14c8d729.js"
+  },
+  {
+    "revision": "e7ee361238612d427c6e",
+    "url": "/js/app.ec20ddfb.js"
+  },
+  {
+    "revision": "945426760e3df45c2251ab9d23a4cfde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4590739eae246b3a17bc",
+    "url": "/css/chunk-vendors.9ed95b7a.css"
+  },
+  {
+    "revision": "e7ee361238612d427c6e",
+    "url": "/css/app.4e8e286f.css"
+  }
+];
---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="darken-1 empty-layout login-wrap" >
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import messages from '@/utils/messages'
+
+export default {
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Что-то пошло не так')
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .login-wrap {
+   background: url("../img/back_auth.png");
+ }
+
+ 
+</style>
